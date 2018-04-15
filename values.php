6e70ecadeb68f0100894f83ea21c97a3984a3d71
bakshi23 <37874573+bakshi23@users.noreply.github.com>
2018-04-15 17:42:13 +0530

    Update values.php

--- values.php
+++ values.php
@@ -1,5 +1,5 @@
 <?php
-  $conn = new mysqli('mydatabase.demo.abc', 'admin', 'admin123', 'mysql');
+  $conn = new mysqli('mydatabase.demo.abc', 'root', 'mymysqlpass', 'mysql');
   $sql = 'SELECT * FROM test';
   $result =$conn->query($sql);
   while($row =$result->fetch_assoc()) { echo 'the value is: ' . $row['firstname'].$row['lastname'] ;}
