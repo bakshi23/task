c788bfa2030c5859c7acde7ecb1251d520ca5511
bakshi23 <37874573+bakshi23@users.noreply.github.com>
2018-04-15 20:09:54 +0530

    Update values.php

--- values.php
+++ values.php
@@ -2,6 +2,6 @@
   $conn = new mysqli('mydatabase.demo.abc', 'root', 'mymysqlpass', 'mysql');
   $sql = 'SELECT * FROM test';
   $result =$conn->query($sql);
-  while($row =$result->fetch_assoc()) { echo 'the value is: ' . $row['firstname'].$row['lastname'] ;}
+  while($row =$result->fetch_assoc()) { echo 'test table contains these is: ' . $row['firstname'].$row['lastname'] ;}
   $conn->close();
   ?>
